16c834f55dec63edbde2423dd5a3e8ed54ea67c2
Jose <josefbautista94@gmail.com>
2025-05-01 01:29:10 -0400

    Implemented the Score component into Learner.jsx

--- src/components/Learner.jsx
+++ src/components/Learner.jsx
@@ -1,8 +1,13 @@
+import Score from "./Score";
+
 function Learner({ learner }) {
   return (
     <div>
       <h2>{learner.name}</h2>
       <p>{learner.bio}</p>
+      {learner.scores.map((score, index) => (
+        <Score key={index} score={score} />
+      ))}
     </div>
   );
 }
